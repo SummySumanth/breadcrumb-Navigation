77e138ff3d330d71cd1814dfba1bc9c3d0f30b3d
SummySumanth <Sumanth261094@gmail.com>
2019-08-27 01:48:45 +0530

    express node server

---
+++ server.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('server started at', process.env.PORT || 3000);
+});
