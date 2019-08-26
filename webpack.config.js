5e0471a00c0ff3997282a9d0e21aba4c43d1b2af
SummySumanth <Sumanth261094@gmail.com>
2019-08-27 01:23:35 +0530

    webpack output path change

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, '/public'),
+    path: __dirname + '/build',
     filename: 'bundle.js'
   },
   module: {
