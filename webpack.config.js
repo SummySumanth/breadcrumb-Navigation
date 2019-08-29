fa13b91c4fd8838c4c267f7179a45ef4eb841e84
SummySumanth <Sumanth261094@gmail.com>
2019-08-29 13:09:02 +0530

    webpack config to make refresh work on any page

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,9 @@ module.exports = {
     filename: 'bundle.js'
   },
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
+    contentBase: './',
+    hot: true
   },
   module: {
     rules:[
